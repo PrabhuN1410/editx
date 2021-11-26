14a72642f11d930ca7beb90309e0f2edaabab314
prabhuN1410 <editxstudios1@gmail.com>
2021-11-26 22:40:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'bootstrap';
+
+function App() {
+  return (
+    <div className="container">
+      <Button>click me</Button>
+    </div>
+  );
+}
+
+export default App;
